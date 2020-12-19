6207c9c744a45ea3580849ded1d06bf3f3762a3a
Jose Valero <josemiguelvaleroreyes@gmail.com>
2020-12-18 21:53:22 -0300

    hacking bootstrap

--- src/ui/pages/Clients/Clients.jsx
+++ src/ui/pages/Clients/Clients.jsx
@@ -5,10 +5,9 @@ const Clients = () => {
   return (
     <div className='container-fluid ' style={{ margin: '50px 0 0 0' }}>
       <div className='row'>
-        <div className='col col-xl-12 col-lg-9 col-md-6 col-sm-3 block_1'>block 1</div>
-        <div className='col col-xl-12 col-lg-9 col-md-6 col-sm-3 block_2'>block 2</div>
-        <div className='col col-xl-12 col-lg-9 col-md-6 col-sm-3 block_3'>block 3</div>
-        <div className='col col-xl-12 col-lg-9 col-md-6 col-sm-3 block_4'>block 4</div>
+        <div className='grid-xl-4 grid-lg-8'>block 2</div>
+        <div className='grid-xl-4 grid-lg-2'>block 3</div>
+        <div className='grid-xl-4 grid-lg-2'>block 4</div>
       </div>
     </div>
   );
