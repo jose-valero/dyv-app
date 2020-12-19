407b0cbdd12d92a7a34cd27b2ab5ba026d2495db
Josue Mendez <josuejoelmm@gmail.com>
2020-12-19 01:15:35 -0300

    cleaning and debugging to _test-grid.scss

--- src/ui/pages/Clients/Clients.jsx
+++ src/ui/pages/Clients/Clients.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Clients.scss';
 
+const styles = {background: 'yellow', border: '1px solid'}
+
 const Clients = () => {
   return (
-    <div className='container-fluid ' style={{ margin: '50px 0 0 0' }}>
+    <div className='container-fluid' style={{marginTop: '50px'}}>
       <div className='row'>
-        <div className='grid-xl-4 grid-lg-8'>block 2</div>
-        <div className='grid-xl-4 grid-lg-2'>block 3</div>
-        <div className='grid-xl-4 grid-lg-2'>block 4</div>
+        <div style={styles} className='col_auto grid-lg-8'>block 1</div>
+        <div style={styles} className='col_auto grid-lg-2'>block 2</div>
+        <div style={styles} className='col_auto grid-lg-2'>block 3</div>
       </div>
     </div>
   );
