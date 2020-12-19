7d5e0c37b42e7343978dd24a7119efa5685d8190
Josue Mendez <josuejoelmm@gmail.com>
2020-12-19 01:30:57 -0300

    unmute important css

--- src/ui/pages/Clients/Clients.jsx
+++ src/ui/pages/Clients/Clients.jsx
@@ -5,7 +5,7 @@ const styles = {background: 'yellow', border: '1px solid'}
 
 const Clients = () => {
   return (
-    <div className='container-fluid' style={{marginTop: '50px'}}>
+    <div className='container' style={{marginTop: '50px'}}>
       <div className='row'>
         <div style={styles} className='col_auto grid-lg-8'>block 1</div>
         <div style={styles} className='col_auto grid-lg-2'>block 2</div>
