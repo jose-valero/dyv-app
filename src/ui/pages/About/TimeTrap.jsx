31b5169d2a36691e527f9f7988c8320d0626086e
Jose Valero <josemiguelvaleroreyes@gmail.com>
2020-12-28 16:06:32 -0300

    alternative timeline, to be checked
    alternative timeline, to be checked

---
+++ src/ui/pages/About/TimeTrap.jsx
@@ -0,0 +1,188 @@
+import './TimeTrap.scss';
+
+const TimeTrap = () => {
+  return (
+    <section id='cd-timeline' class='cd-container'>
+      <div class='cd-timeline-block'>
+        <div class='cd-timeline-img cd-picture'></div>
+
+        <div class='cd-timeline-content'>
+          <h2>HeadLine</h2>
+          <div class='timeline-content-info'>
+            <span class='timeline-content-info-title'>
+              <i class='fa fa-certificate' aria-hidden='true'></i>
+              Title sub-title
+            </span>
+            <span class='timeline-content-info-date'>
+              <i class='fa fa-calendar-o' aria-hidden='true'></i>
+              date_1 - date_2
+            </span>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+            facere, neque unde tempore, ea soluta quidem dolores vel dolor
+          </p>
+          <span class='cd-date'>2010</span>
+          <ul class='content-skills'>
+            <li>item#1</li>
+            <li>item#2</li>
+            <li>item#3</li>
+            <li>item#4</li>
+            <li>item#5</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class='cd-timeline-block'>
+        <div class='cd-timeline-img cd-movie'></div>
+
+        <div class='cd-timeline-content'>
+          <h2>HeadLine</h2>
+          <div class='timeline-content-info'>
+            <span class='timeline-content-info-title'>
+              <i class='fa fa-certificate' aria-hidden='true'></i>
+              title_1 | title_2
+            </span>
+            <span class='timeline-content-info-date'>
+              <i class='fa fa-calendar-o' aria-hidden='true'></i>
+              date_1 | date_2
+            </span>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+            facere, neque unde tempore, ea soluta quidem dolores vel dolor
+          </p>
+          <span class='cd-date'>2010</span>
+          <ul class='content-skills'>
+            <li>Item#1</li>
+            <li>item#2</li>
+            <li>item#3</li>
+            <li>item#4</li>
+            <li>item#5</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class='cd-timeline-block'>
+        <div class='cd-timeline-img cd-picture'></div>
+
+        <div class='cd-timeline-content'>
+          <h2>HeadLine</h2>
+          <div class='timeline-content-info'>
+            <span class='timeline-content-info-title'>
+              <i class='fa fa-certificate' aria-hidden='true'></i>
+              title_1 | title_2
+            </span>
+            <span class='timeline-content-info-date'>
+              <i class='fa fa-calendar-o' aria-hidden='true'></i>
+              date_1 | date_2
+            </span>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+            facere, neque unde tempore, ea soluta quidem dolores vel dolor
+          </p>
+          <span class='cd-date'>2010</span>
+          <ul class='content-skills'>
+            <li>item#1</li>
+            <li>item#2</li>
+            <li>item#3</li>
+            <li>item#4</li>
+            <li>item#5</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class='cd-timeline-block'>
+        <div class='cd-timeline-img cd-location'></div>
+        <div class='cd-timeline-content'>
+          <h2>HeadLine</h2>
+          <div class='timeline-content-info'>
+            <span class='timeline-content-info-title'>
+              <i class='fa fa-certificate' aria-hidden='true'></i>
+              title_1 | title_2
+            </span>
+            <span class='timeline-content-info-date'>
+              <i class='fa fa-calendar-o' aria-hidden='true'></i>
+              date_1 | date_2
+            </span>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+            facere, neque unde tempore, ea soluta quidem dolores vel dolor
+          </p>
+          <span class='cd-date'>2010</span>
+          <ul class='content-skills'>
+            <li>item#1</li>
+            <li>item#2</li>
+            <li>item#3</li>
+            <li>item#4</li>
+            <li>item#5</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class='cd-timeline-block'>
+        <div class='cd-timeline-img cd-location'></div>
+
+        <div class='cd-timeline-content'>
+          <h2>HeadLine</h2>
+          <div class='timeline-content-info'>
+            <span class='timeline-content-info-title'>
+              <i class='fa fa-certificate' aria-hidden='true'></i>
+              title_1 | title_2
+            </span>
+            <span class='timeline-content-info-date'>
+              <i class='fa fa-calendar-o' aria-hidden='true'></i>
+              date_1 | date_2
+            </span>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+            facere, neque unde tempore, ea soluta quidem dolores vel dolor
+          </p>
+          <span class='cd-date'>2010</span>
+          <ul class='content-skills'>
+            <li>item#1</li>
+            <li>item#2</li>
+            <li>item#3</li>
+            <li>item#4</li>
+            <li>item#5</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class='cd-timeline-block'>
+        <div class='cd-timeline-img cd-movie'></div>
+
+        <div class='cd-timeline-content'>
+          <h2>HeadLine</h2>
+          <div class='timeline-content-info'>
+            <span class='timeline-content-info-title'>
+              <i class='fa fa-certificate' aria-hidden='true'></i>
+              title_1 | title_2
+            </span>
+            <span class='timeline-content-info-date'>
+              <i class='fa fa-calendar-o' aria-hidden='true'></i>
+              date_1 | date_2
+            </span>
+          </div>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim
+            facere, neque unde tempore, ea soluta quidem dolores vel dolor
+          </p>
+          <span class='cd-date'>2010</span>
+          <ul class='content-skills'>
+            <li>item#1</li>
+            <li>item#2</li>
+            <li>item#3</li>
+            <li>item#4</li>
+            <li>item#5</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TimeTrap;
