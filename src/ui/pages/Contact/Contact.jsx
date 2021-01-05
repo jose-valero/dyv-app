b8af8847ad0c7c687ca995d99734be4f114b766e
Josue Mendez <josuejoelmm@gmail.com>
2021-01-04 23:57:23 -0300

    update contact page

--- src/ui/pages/Contact/Contact.jsx
+++ src/ui/pages/Contact/Contact.jsx
@@ -8,36 +8,41 @@ const Contact = () => {
         <div className="contact__container">
             <div className="dyv__container-fluid form__container no_padding">
                 <div className="dyv__row">
-                    <div className="dyv__col-6 no_padding">
+                    <div className="dyv__col-6 no_padding" background>
                         <figure>
                             <img src={bgFormImage} alt=""/>
                         </figure>
                     </div>
                     <div className="dyv__col-6">
-                        <div className="dyv__row">
+                        <div className="inner_form_wrapper dyv__row">
                             <div className="dyv__col">
-                                <h1 className="text_center">CONTACTANOS</h1>
+                                <div className="dyv__row">
+                                    <div className="dyv__col">
+                                        <h1 className="text_center">CONTACTANOS</h1>
+                                    </div>
+                                </div>
+                                <div className="dyv__row">
+                                    <div className="dyv__col">
+                                        <InputText name="username" label="Nombre" />
+                                    </div>
+                                </div>
+                                <div className="dyv__row">
+                                    <div className="dyv__col">
+                                        <InputText name="username" label="Apellido" />
+                                    </div>
+                                </div>
+                                <div className="dyv__row">
+                                    <div className="dyv__col">
+                                        <textarea className="full_w" name="msg" id="" cols="30" rows="10"></textarea>
+                                    </div>
+                                </div>
+                                <div className="dyv__row">
+                                    <div className="dyv__col">
+                                        <Button name="ENVIAR" route="/contact" />
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                        <div className="dyv__row">
-                            <div className="dyv__col">
-                                <InputText label="Nombre" />
-                            </div>
-                        </div>
-                        <div className="dyv__row">
-                            <div className="dyv__col">
-                                <InputText label="Apellido" />
-                            </div>
-                        </div>
-                        <div className="dyv__row">
-                            <div className="dyv__col">
-                                <textarea className="full_w" name="msg" id="" cols="30" rows="10"></textarea>
-                            </div>
-                        </div>
-                        <div className="dyv__row">
-                            <div className="dyv__col">
-                                <Button name="ENVIAR" route="/contact" />
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
--- src/ui/pages/Contact/InputText/InputText.jsx
+++ src/ui/pages/Contact/InputText/InputText.jsx
@@ -1,7 +1,6 @@
 import './style.scss';
 
-function InputText({ label }) {
-
+function InputText({ id, label }) {
     const onInputClick = (e) => {
         e.target.parentElement.previousElementSibling.classList.toggle('InputLabel-shrink');
         e.target.parentElement.previousElementSibling.classList.toggle('InputLabel-focused');
@@ -12,7 +11,13 @@ function InputText({ label }) {
         <div className="input__text__wrapper">
             <label className="InputLabel-root InputLabel-animated InputLabel-formControl">{label}</label>
             <div className="input__inner">
-                <input className="full_w" type="text" onClick={onInputClick} />
+                <input 
+                    id={id && id} 
+                    className="full_w" 
+                    type="text" 
+                    onFocus={onInputClick} 
+                    onBlur={onInputClick}
+                />
             </div>
         </div>
     )
