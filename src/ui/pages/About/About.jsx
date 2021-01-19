adc5df4dd53617710534daa0172edabf29c4dbd7
Jose Valero <josemiguelvaleroreyes@gmail.com>
2021-01-19 14:59:30 -0300

    about fix

--- src/ui/pages/About/About.jsx
+++ src/ui/pages/About/About.jsx
@@ -1,31 +1,32 @@
-// import React from 'react';
-
 import { Language } from '../../../data/data';
 import { useSelector } from 'react-redux';
 import Values from './Values';
-import Mission from './Mission';
 import { PageContainer } from '../../components/Container/PageContainer';
 import Hero from '../../components/Hero/Hero';
 import AOS from 'aos';
 import Bio from './Bio';
 import NextView from '../../components/NextView/NextView';
+import Description from '../../components/BannerDescription/Description';
 import './About.scss';
 
 const About = () => {
   const lang = useSelector((state) => state.langReducer);
-  const text = Language[`${lang}`].about.nextPage;
+  const nextPageText = Language[`${lang}`].about.nextPage;
+  const misionTitle = Language[`${lang}`].about.banner_missionTitle;
+  const missionText = Language[`${lang}`].about.banner_missionText;
+  const valueTitle = Language[`${lang}`].about.banner_valuesTitle;
+  const valueText = Language[`${lang}`].about.banner_valuesText;
   AOS.init();
   return (
     <>
       {/* colocar todos los props aqui ___________ */}
       <PageContainer>
         <Hero />
-        <>
-          <Bio />
-          <Mission />
-          <Values />
-          <NextView nextTo={text} goTo='/services' />
-        </>
+        <Bio />
+        <Description title={misionTitle} text={missionText} />
+        <Values />
+        <Description title={valueTitle} text={valueText} />
+        <NextView nextTo={nextPageText} goTo='/services' />
       </PageContainer>
     </>
   );
--- src/ui/pages/About/Values.jsx
+++ src/ui/pages/About/Values.jsx
@@ -1,5 +1,3 @@
-import { useSelector } from 'react-redux';
-import { Language } from '../../../data/data';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -7,13 +5,8 @@ import Image from 'react-bootstrap/Image';
 import kitchen from './assets/kitchen.jpg';
 import living from './assets/living.jpg';
 import rest from './assets/rest.jpg';
-import Description from './Description';
 
 const Values = () => {
-  const lang = useSelector((state) => state.langReducer);
-  const title = Language[`${lang}`].about.banner_valuesTitle;
-  const text = Language[`${lang}`].about.banner_valuesText;
-
   return (
     <Container className='about__mission-container'>
       <Row className='about__mission-row'>
@@ -73,11 +66,6 @@ const Values = () => {
               <h2>Text</h2>
             </div>
           </div>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Description title={title} text={text} />
         </Col>
       </Row>
     </Container>
