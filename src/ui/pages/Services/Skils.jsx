f194146a963eff87942600fea5cdfb37f2475ac8
Jose Valero <josemiguelvaleroreyes@gmail.com>
2021-01-20 14:03:28 -0300

    skills grid

--- src/ui/pages/Services/Skils.jsx
+++ src/ui/pages/Services/Skils.jsx
@@ -3,14 +3,15 @@ import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import skillImg from '../../../assets/images/2.jpg';
+import { FaAccusoft, FaBlackTie, FaCanadianMapleLeaf } from 'react-icons/fa';
 import './Skills.scss';
 
 const Skils = () => {
   return (
-    <Container className=' '>
+    <Container className='skills__container '>
       <Row className='row align-middle'>
-        <Col md='6' lg='4' className='column'>
-          <Card className=''>
+        <Col sm='6' md='4' lg='4' xl='4' className='column'>
+          <Card className='card gr-1'>
             <div className='txt'>
               <h1>
                 BRANDING AND <br />
@@ -19,14 +20,12 @@ const Skils = () => {
               <p>Visual communication and problem-solving</p>
             </div>
             <a href='#'>more</a>
-            <div className='ico-card'>
-              <img src={skillImg} className='w-100 p-0 m-0' />
-              {/* <i className='fa fa-rebel'></i> */}
-            </div>
+            {/* <div className='ico-card'>
+              <FaCanadianMapleLeaf className='icox' />
+            </div> */}
           </Card>
         </Col>
-
-        <Col md='6' lg='4' className='column'>
+        <Col sm='6' md='4' lg='4' xl='4' className='column'>
           <div className='card gr-2'>
             <div className='txt'>
               <h1>
@@ -37,12 +36,11 @@ const Skils = () => {
             </div>
             <a href='#'>more</a>
             <div className='ico-card'>
-              <img src={skillImg} className='w-100' />
-              {/* <i className='fa fa-codepen'></i> */}
+              <FaBlackTie className='icox' />
             </div>
           </div>
         </Col>
-        <Col md='6' lg='4' className='column'>
+        <Col sm='6' md='4' lg='4' xl='4' className='column'>
           <div className='card gr-3'>
             <div className='txt'>
               <h1>
@@ -52,8 +50,52 @@ const Skils = () => {
             </div>
             <a href='#'>more</a>
             <div className='ico-card'>
-              <img src={skillImg} className='w-100' />
-              {/* <i className='fa fa-empire'></i> */}
+              <FaAccusoft className='icox' />
+            </div>
+          </div>
+        </Col>
+        <Col sm='6' md='4' lg='4' xl='4' className='column'>
+          <Card className='gr-4'>
+            <div className='txt'>
+              <h1>
+                BRANDING AND <br />
+                CORPORATE DESIGN
+              </h1>
+              <p>Visual communication and problem-solving</p>
+            </div>
+            <a href='#'>more</a>
+            <div className='ico-card'>
+              <FaAccusoft className='icox' />
+            </div>
+          </Card>
+        </Col>
+
+        <Col sm='6' md='4' lg='4' xl='4' className='column'>
+          <div className='card gr-5'>
+            <div className='txt'>
+              <h1>
+                Web Front-End <br />
+                SOLUTIONS
+              </h1>
+              <p>How design is implemented on the web.</p>
+            </div>
+            <a href='#'>more</a>
+            <div className='ico-card'>
+              <FaBlackTie className='icox' />
+            </div>
+          </div>
+        </Col>
+        <Col sm='6' md='4' lg='4' xl='4' className='column'>
+          <div className='card gr-6'>
+            <div className='txt'>
+              <h1>
+                UX/UI WEBsite <br /> AND MOBILE app
+              </h1>
+              <p>User Interface and User Experience Design.</p>
+            </div>
+            <a href='#'>more</a>
+            <div className='ico-card'>
+              <FaCanadianMapleLeaf className='icox' />
             </div>
           </div>
         </Col>
