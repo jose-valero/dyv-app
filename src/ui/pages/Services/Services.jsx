deb2e5e2ff24643789aa51b4997e22fa5026ae7a
Jose Valero <josemiguelvaleroreyes@gmail.com>
2021-01-27 18:46:49 -0300

    mockup services

--- src/ui/pages/Services/Services.jsx
+++ src/ui/pages/Services/Services.jsx
@@ -1,21 +1,32 @@
 // import { useSelector } from 'react-redux';
 import { PageContainer } from '../../components/Container/PageContainer';
 import CarouselPage from './CarouselPage';
-// import { Language } from '../../../data/data';
+import { useSelector } from 'react-redux';
+import NextView from '../../components/NextView/NextView';
+import { Language } from '../../../data/data';
 import Hero from '../../components/Hero/Hero';
 import Skils from './Skils';
-import './Services.scss';
 import Description from '../../components/BannerDescription/Description';
+import './Services.scss';
 
 const Services = () => {
-  // const lang = useSelector((state) => state.langReducer);
+  const lang = useSelector((state) => state.langReducer);
+  const nextPageText = Language[`${lang}`].services.nextPage;
+
   return (
     <PageContainer>
       <Hero />
-      <Description text="text" title="title"/>
+      <Description
+        text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae cum impedit quisquam vel accusantium ducimus, necessitatibus nam aliquam atque esse repellendus blanditiis quos totam omnis quo enim. Doloribus, alias perferendis?'
+        title='title'
+      />
       <CarouselPage />
-      <Description text="text" title="title"/>
+      <Description
+        text='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae cum impedit quisquam vel accusantium ducimus, necessitatibus nam aliquam atque esse repellendus blanditiis quos totam omnis quo enim. Doloribus, alias perferendis?'
+        title='title'
+      />
       <Skils />
+      <NextView nextTo={nextPageText} goTo={'/portafolio'} />
     </PageContainer>
   );
 };
--- src/ui/pages/Services/Skils.jsx
+++ src/ui/pages/Services/Skils.jsx
@@ -1,104 +1,151 @@
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
+import Image from 'react-bootstrap/Image';
+
+// import Card from 'react-bootstrap/Card';
 import skillImg from '../../../assets/images/2.jpg';
-import { FaAccusoft, FaBlackTie, FaCanadianMapleLeaf } from 'react-icons/fa';
+// import { FaAccusoft, FaBlackTie, FaCanadianMapleLeaf } from 'react-icons/fa';
+// import { GiTechnoHeart, GiCyberEye, GiFountain } from 'react-icons/gi';
+// import { RiHome2Line } from 'react-icons/ri';
 import './Skills.scss';
 
 const Skils = () => {
-  return (
-    <Container className='skills__container '>
-      <Row className='row align-middle'>
-        <Col sm='6' md='4' lg='4' xl='4' className='column'>
-          <Card className='card gr-1'>
-            <div className='txt'>
-              <h1>
-                BRANDING AND <br />
-                CORPORATE DESIGN
-              </h1>
-              <p>Visual communication and problem-solving</p>
-            </div>
-            <a href='#'>more</a>
-            {/* <div className='ico-card'>
-              <FaCanadianMapleLeaf className='icox' />
-            </div> */}
-          </Card>
-        </Col>
-        <Col sm='6' md='4' lg='4' xl='4' className='column'>
-          <div className='card gr-2'>
-            <div className='txt'>
-              <h1>
-                Web Front-End <br />
-                SOLUTIONS
-              </h1>
-              <p>How design is implemented on the web.</p>
-            </div>
-            <a href='#'>more</a>
-            <div className='ico-card'>
-              <FaBlackTie className='icox' />
-            </div>
-          </div>
-        </Col>
-        <Col sm='6' md='4' lg='4' xl='4' className='column'>
-          <div className='card gr-3'>
-            <div className='txt'>
-              <h1>
-                UX/UI WEBsite <br /> AND MOBILE app
-              </h1>
-              <p>User Interface and User Experience Design.</p>
-            </div>
-            <a href='#'>more</a>
-            <div className='ico-card'>
-              <FaAccusoft className='icox' />
-            </div>
-          </div>
-        </Col>
-        <Col sm='6' md='4' lg='4' xl='4' className='column'>
-          <Card className='gr-4'>
-            <div className='txt'>
-              <h1>
-                BRANDING AND <br />
-                CORPORATE DESIGN
-              </h1>
-              <p>Visual communication and problem-solving</p>
-            </div>
-            <a href='#'>more</a>
-            <div className='ico-card'>
-              <FaAccusoft className='icox' />
-            </div>
-          </Card>
-        </Col>
 
-        <Col sm='6' md='4' lg='4' xl='4' className='column'>
-          <div className='card gr-5'>
-            <div className='txt'>
-              <h1>
-                Web Front-End <br />
-                SOLUTIONS
-              </h1>
-              <p>How design is implemented on the web.</p>
-            </div>
-            <a href='#'>more</a>
-            <div className='ico-card'>
-              <FaBlackTie className='icox' />
-            </div>
-          </div>
+
+  return (
+    // <Container className='skills__container '>
+    //   <Row className='row align-middle'>
+    //     <Col sm='12' md='4' md='4' xl='4' className='column'>
+    //       <Card className='card gr-1'>
+    //         <div className='txt'>
+    //           <h1>
+    //             BRANDING AND <br />
+    //             CORPORATE DESIGN
+    //           </h1>
+    //           <p>Visual communication and problem-solving</p>
+    //         </div>
+    //         <a href='#'>more</a>
+    //         <div className='ico-card'>
+    //           {/* <GiTechnoHeart className='icox' /> */}
+    //         </div>
+    //       </Card>
+    //     </Col>
+    //     <Col sm='12' md='4' md='4' xl='4' className='column'>
+    //       <div className='card gr-2'>
+    //         <div className='txt'>
+    //           <h1>
+    //             We do Home <br />
+    //             SOLUTIONS
+    //           </h1>
+    //           <p>How design is implemented on the web.</p>
+    //         </div>
+    //         <a href='#'>more</a>
+    //         <div className='ico-card'>
+    //           <RiHome2Line className='icox' />
+    //         </div>
+    //       </div>
+    //     </Col>
+    //     <Col sm='12' md='4' md='4' xl='4' className='column'>
+    //       <div className='card gr-3'>
+    //         <div className='txt'>
+    //           <h1>
+    //             Exterior <br /> Design
+    //           </h1>
+    //           <p>User Interface and User Experience Design.</p>
+    //         </div>
+    //         <a href='#'>more</a>
+    //         <div className='ico-card'>
+    //           {/* <GiFountain className='icox' /> */}
+    //         </div>
+    //       </div>
+    //     </Col>
+    //   </Row>
+    // </Container>
+
+    <Container className='skills__container'>
+      {/* ROW 1 */}
+      <Row className='skills__container--row '>
+        <Col md={6} className='skills__container--row--left'>
+          <Image src={skillImg} className='skills__images' />
         </Col>
-        <Col sm='6' md='4' lg='4' xl='4' className='column'>
-          <div className='card gr-6'>
-            <div className='txt'>
-              <h1>
-                UX/UI WEBsite <br /> AND MOBILE app
-              </h1>
-              <p>User Interface and User Experience Design.</p>
-            </div>
-            <a href='#'>more</a>
-            <div className='ico-card'>
-              <FaCanadianMapleLeaf className='icox' />
-            </div>
-          </div>
+        <Col md={6} className='skills__container--row--right m-auto'>
+          <p className='text-center m-0'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae cum
+            impedit quisquam vel accusantium ducimus, necessitatibus nam aliquam
+            atque esse repellendus blanditiis quos totam omnis quo enim.
+            Doloribus, alias perferendis?
+          </p>
         </Col>
+      </Row>
+
+      {/* ROW 2 */}
+      <Row className='skills__container--row'>
+        <Col
+          lg={4}
+          md={4}
+          sm={12}
+          xs={12}
+          className='skills__container--row--left m-auto'
+        >
+          <p className='text-center m-0'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae cum
+            impedit quisquam vel accusantium ducimus, necessitatibus nam aliquam
+            atque esse repellendus blanditiis quos totam omnis quo enim.
+            Doloribus, alias perferendis?
+          </p>
+        </Col>
+        <Col
+          lg={4}
+          md={4}
+          sm={5}
+          xs={6}
+          className='skills__container--row--right m-auto'
+        >
+          <Image src={skillImg} className='skills__images' />
+        </Col>
+        <Col
+          lg={4}
+          md={4}
+          sm={5}
+          xs={6}
+          className='skills__container--row--right m-auto'
+        >
+          <Image src={skillImg} className='skills__images' />
+        </Col>
+      </Row>
+
+      {/* ROW 3 */}
+      <Row className='skills__container--row'>
+        <Col md={7} className='skills__container--row--right m-auto'>
+          <Image src={skillImg} className='skills__images' />
+        </Col>
+        <Col md={5} className='skills__container--row--left m-auto'>
+          <p className='text-center m-0'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae cum
+            impedit quisquam vel accusantium ducimus, necessitatibus nam aliquam
+            atque esse repellendus blanditiis quos totam omnis quo enim.
+            Doloribus, alias perferendis?
+          </p>
+        </Col>
+      </Row>
+
+      {/* ROW 4 */}
+      <Row className='skills__container--row'>
+        <Col md={4} className='skills__container--row--left m-auto'>
+          <p className='text-center m-0'>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vitae cum
+            impedit quisquam vel accusantium ducimus, necessitatibus nam aliquam
+            atque esse repellendus blanditiis quos totam omnis quo enim.
+            Doloribus, alias perferendis?
+          </p>
+        </Col>
+        <Col md={8} className='skills__container--row--right m-auto'>
+          <Image src={skillImg} className='skills__images' />
+        </Col>
+        {/* <Col md={3} className='skills__container--row--right m-auto'>
+          <Image src={skillImg} className='skills__images' />
+        </Col> */}
       </Row>
     </Container>
   );
