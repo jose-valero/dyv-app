e1eeaabd5c3dd3e6fa87597c8f3a0909b09ceee7
Jose Valero <josemiguelvaleroreyes@gmail.com>
2021-03-23 21:49:12 -0300

    portafolio details

--- src/ui/pages/Portafolio/Portafolio.jsx
+++ src/ui/pages/Portafolio/Portafolio.jsx
@@ -1,51 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { Language } from "../../../data/index";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
-import Card from "react-bootstrap/Card";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { Language } from '../../../data/index';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
 
+import { Description } from '../../components';
+import PlacesDetails from './PlacesDetails';
 
-import {  Description } from '../../components';
 import { PageContainer } from '../../components/Container/PageContainer';
 
 // import Hero from "../../components/Hero/Hero";
 
-import MAP from "./assets/bg-portafolio-2.png";
-import C_006 from "./assets/C_006.jpg";
-import "./Portafolio.scss";
+import MAP from './assets/bg-portafolio-2.png';
+import C_006 from './assets/C_006.jpg';
+import './Portafolio.scss';
 
 const Portafolio = () => {
   const lang = useSelector((state) => state.langReducer);
   const places = [
-    "LAS BAHAMAS",
-    "VENEZUELA",
-    "PUERTO RICO",
-    "MIAMI, USA",
-    "NEW YORK, USA",
-    "MIAMI, USA",
+    'LAS BAHAMAS',
+    'VENEZUELA',
+    'PUERTO RICO',
+    'MIAMI, USA',
+    'NEW YORK, USA',
+    'MIAMI, USA',
   ];
 
   return (
     <PageContainer>
       {/* <Hero displayOnPage='portafolio' /> */}
-      <Container className="portfolio__page">
+      <Container className='portfolio__page'>
         <Description
           title={Language[`${lang}`].portafolio.descriptionHeadline}
           text={Language[`${lang}`].portafolio.descriptionSubhead}
         />
         <Image src={MAP} fluid />
-        <Row className="portafolio__card pb-4">
+        <Row className='portafolio__card pb-4'>
           {places.map((clientPlace) => (
-            <Col xs="4" className="portafolio__card--content">
-              <Link to="/portafolio">
-                <Card className="portafolio__card--content---cards">
-                  <Card.Img variant="top" src={C_006} />
-                  <Card.Body className="mx-1 px-0">
-                    <Card.Text className="portafolio__card--text">
+            <Col xs='4' className='portafolio__card--content'>
+              <Link to='/portafolio'>
+                <Card className='portafolio__card--content---cards'>
+                  <Card.Img variant='top' src={C_006} />
+                  <Card.Body className='mx-1 px-0'>
+                    <Card.Text className='portafolio__card--text'>
                       {clientPlace}
                     </Card.Text>
                   </Card.Body>
@@ -55,6 +56,7 @@ const Portafolio = () => {
           ))}
         </Row>
       </Container>
+      <PlacesDetails />
     </PageContainer>
   );
 };
--- src/ui/pages/Portafolio/placesDetails.jsx
+++ src/ui/pages/Portafolio/placesDetails.jsx
@@ -1,18 +1,63 @@
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Badge from 'react-bootstrap/Badge';
 import { PageContainer } from '../../components/Container/PageContainer';
-
-const placesDetails = () => {
+import Image from 'react-bootstrap/Image';
+import imgTest from './assets/C_006.jpg';
+const PlacesDetails = () => {
   return (
     <PageContainer>
-      <Container>
-        <Row>
-          <Col>DETAIL OF PORTAFOLIO</Col>
+      <Container className='portafolioDetail__container'>
+        <h1 className='text-center'>HOLA PROYECTO #n</h1>
+        <Row className='portafolioDetail__topsection'>
+          <Col className='portafolioDetail__topsection-title'>
+            LISTA DE DETALLES
+            <ul>
+              <li>
+                especificacion del proyecto #1, puede ser otra cosa tecnica o
+                nose
+              </li>
+              <li>
+                especificacion del proyecto #2, puede ser otra cosa tecnica o
+                nose
+              </li>
+              <li>
+                especificacion del proyecto #3, puede ser otra cosa tecnica o
+                nose
+              </li>
+              <li>
+                especificacion del proyecto #4, puede ser otra cosa tecnica o
+                nose
+              </li>
+              <li>
+                especificacion del proyecto #5, puede ser otra cosa tecnica o
+                nose
+              </li>
+            </ul>
+          </Col>
+          <Col className='portafolioDetail__topsection-images'>
+            <Image src={imgTest} fluid />
+          </Col>
+        </Row>
+        <Row className='portafolioDetail__botsection'>
+          <Col lg={12} className='portafolioDetail__botsection-desc'>
+            <b>Descripcion del proyecto:</b> Lorem ipsum dolor sit amet
+            consectetur adipisicing elit. Ab id atque a eum ut, soluta
+            perspiciatis unde fuga accusamus, provident nobis asperiores
+            repellat praesentium, repudiandae sit porro voluptatibus ratione
+            iste?
+          </Col>
+          <Col lg={12} className='portafolioDetail__botsection-utils'>
+            <a href='#' className="px-1 mx-1">Descarga</a>
+            <Badge variant='primary' className="px-1 mx-1">2018</Badge>
+            <Badge variant='primary' className="px-1 mx-1">keyword</Badge>
+            <Badge variant='primary' className="px-1 mx-1" >Download</Badge>
+          </Col>
         </Row>
       </Container>
     </PageContainer>
   );
 };
 
-export default placesDetails;
+export default PlacesDetails;
