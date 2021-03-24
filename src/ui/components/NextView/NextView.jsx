f8661220bb1cf7fa6c2c8c4c43b73c2b31e81e13
Jose Valero <josemiguelvaleroreyes@gmail.com>
2021-03-23 21:03:41 -0300

    fix styles into the nextview component

--- src/ui/components/NextView/NextView.jsx
+++ src/ui/components/NextView/NextView.jsx
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
+import { useHistory } from 'react-router-dom';
+import _variables from "../../../styles/_variables.module.scss"
 
 export const PageLinkContainer = styled.div`
   margin: 3% 10%;
   text-align: right;
 `;
 export const PageLink = styled.h4`
-  color: #dda987;
+  color: ${_variables.pal_1_oj_2};
   transition: all 0.3s ease-out;
   &:hover {
-    color: #cfad97;
+    color: ${_variables.pal_1_oj_1};
     transform: scale(1.1);
     transition: all 0.3s ease-out;
     text-decoration: none;
@@ -19,16 +21,22 @@ export const PageLink = styled.h4`
     font-size: 16px;
   }
 `;
+export const StyledLink = styled(NavLink)`
+  text-decoration: none;
+`;
 
 const NextView = ({ nextTo, goTo }) => {
+  let history = useHistory();
+  console.log('history', history);
+
   return (
     <PageLinkContainer>
-      <Link to={goTo} style={{ textDecoration: 'none' }}>
+      <StyledLink to={goTo}>
         <PageLink>
           {nextTo}
           <BsArrowRight className='ml-2' />
         </PageLink>
-      </Link>
+      </StyledLink>
     </PageLinkContainer>
   );
 };
